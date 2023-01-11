3d036d01b03737296874cd27563fc370a7ee2c97
胡小右 <huxiaoyou888@gamil.com>
2023-01-11 15:53:39 +0800

    add

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // 解决与element样式冲突
+    function ({ addBase }) {
+      addBase({
+        '.el-button': {
+          'background-color': 'var(--el-button-bg-color,val(--el-color-white))',
+        },
+      });
+    },
+  ],
+};
